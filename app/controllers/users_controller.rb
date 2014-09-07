f9a032166f2759fa095d318faf5590d91ce4d457
Tunehiko511 <simanezumi1989@gmail.com>
2014-09-07 22:35:54 +0900

    Initialize repository

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :signed_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -7,10 +7,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+   def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -23,5 +36,11 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before actions
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905144437) do
+ActiveRecord::Schema.define(version: 20140906153933) do
 
   create_table "users", force: true do |t|
     t.string   "name"
